84b8a9fd1eeec60b3f3462d55c23d727d92f2bde
Jonathan Saylor <jsaylor525@gmail.com>
2018-11-04 13:05:51 -0600

    Reorganized the css from theme to css folder

--- nomad/workout/static/js/workoutBuilder.js
+++ nomad/workout/static/js/workoutBuilder.js
@@ -1,21 +1,29 @@
 var i = 0;
-var original = document.getElementById('movementEntry');
+
+// Store the originalMovement as a template
+var originalMovement = document.getElementById('movementEntry');
 
 function addNewMovement() {
-   var clone = original.cloneNode(true); // "deep" clone
+   var clone = originalMovement.cloneNode(true); // "deep" clone
    clone.id = "movementEntry" + ++i; // there can only be one element with an ID
-   original.parentNode.appendChild(clone);  
+   originalMovement.parentNode.appendChild(clone);  
 }
 
 function removeMovement(obj) {
    var node = obj
-   var nodeToRemove = node.parentNode.parentNode
-   if ( original.id === nodeToRemove.parentElement.id ) {
+   var nodeToRemove = node.parentNode.parentNode.parentNode
+   if ( originalMovement.id === nodeToRemove.parentElement.id ) {
       console.log("Attempted to remove last movement")
    }
    else {
       nodeToRemove.parentNode.remove(nodeToRemove)
    }
+}
+
+var originalWorkout = document.getElementById('movementEntry');
+
+function addNewWorkoutSection() {
+
 }
 
 $( function() {
