5c4ac438a5b25f856fbee0cd2544352d726c46a5
Jonathan Saylor <jonathan.saylor@raytheon.com>
2019-03-03 20:16:51 -0600

    Working on updating the clock application
    Using a open source digital clock now
    
    Refs #2

--- nomad/workout/static/js/clock.js
+++ nomad/workout/static/js/clock.js
@@ -1,89 +1,172 @@
+class Time {
+  constructor() {
+    this.milliseconds = Date.now()
+  }
 
- var startTime = 0
- var timerEnable = false
- var intervalOn = 0.0
- var intervalOff = 0.0
- var timeCap = 0.0
+  get ms() {
+    return milliseconds % 1000;
+  }
 
- document.getElementById("clock").addEventListener( 'onload', updateClock);
- document.getElementById("start-btn").addEventListener( 'click', startTimer)
- document.getElementById("stop-btn").addEventListener( 'click', stopTimer)
+  get s() {
+    return Math.floor(milliseconds / 1000) % 60;
+  }
 
- function archiveClock(time) {
+  get m() {
+    return Math.floor(milliseconds / 1000 / 60) % 60;
+  }
+
+  get h() {
+    return Math.floor(milliseconds / 1000 / 60 / 60);
+  }
+
+  get rawTime() {
+    return milliseconds;
+  }
+  
+  get millisecondDigits() {
+    return [this.ms % 10, Math.floor(this.ms / 10) % 10, Math.floor(this.ms / 100) ];
+  }
+
+  get millisecondDigit(digit) {
+    return this.millisecondDigits[digits];
+  }
+  
+  get secondDigits() {
+    return [this.s % 10, Math.floor(this.s / 10) % 10];
+  }
+
+  get secondDigit(digit) {
+    return this.secondDigits[digits];
+  }
+  
+  get minutesDigits() {
+    return [this.m % 10, Math.floor(this.m / 10) % 10];
+  }
+
+  get minutesDigit(digit) {
+    return this.minutesDigits[digits];
+  }
+
+  minus(time) {
+    return (this.milliseconds -= time.rawTime)
+  }
+}
+
+var digitSegments = [
+  [1, 2, 3, 4, 5, 6],
+  [2, 3],
+  [1, 2, 7, 5, 4],
+  [1, 2, 7, 3, 4],
+  [6, 7, 2, 3],
+  [1, 6, 7, 3, 4],
+  [1, 6, 5, 4, 3, 7],
+  [1, 2, 3],
+  [1, 2, 3, 4, 5, 6, 7],
+  [1, 2, 7, 3, 6]
+];
+
+class ClockDigit {
+  constructor(digit) {
+    this.digit = digit;
+  }
+
+  setNumber(value, on = 1) {
+    var current = parseInt(digit.getAttribute('data-value'));
+
+    // only switch if number has changed or wasn't set
+    if (!isNaN(current) && current != value) {
+      // unset previous number
+      digitSegments[current].forEach(function (digitSegment, index) {
+        setTimeout(function () {
+          digit[digitSegment - 1].classList.remove('on');
+        }, index * 45)
+      });
+    }
+
+    if (isNaN(current) || current != number) {
+      // set new number after
+      setTimeout(function () {
+        digitSegments[number].forEach(function (digitSegment, index) {
+          setTimeout(function () {
+            digit[digitSegment - 1].classList.add('on');
+          }, index * 45)
+        });
+      }, 250);
+      digit.setAttribute('data-value', number);
+    }
+  }
+
+  clearNumber() {
+    setNumber(0, on);
+  }
+}
+
+class WodClock {
+  constructor() {
+    this._hours = document.querySelectorAll('.hours');
+    this._minutes = document.querySelectorAll('.minutes');
+    this._seconds = document.querySelectorAll('.seconds');
+    this._milliseconds = document.querySelectorAll('.milliseconds');
+    this._digit = {
+      "hours": [],
+      "minutes": [],
+      "seconds": [],
+      "milliseconds": [],
+    }
+    this._hours.forEach(digit => this._digit["hours"].push(new ClockDigit(digit)));
+    this._minutes.forEach(digit => this._digit["minutes"].push(new ClockDigit(digit)));
+    this._seconds.forEach(digit => this._digit["seconds"].push(new ClockDigit(digit)));
+    this._milliseconds.forEach(digit => this._digit["milliseconds"].push(new ClockDigit(digit)));
+    this._startTime = null;
+    this._curretTime = null;
+    this._clockEnable = false;
+  }
+
+  startClock() {
+    this._clockEnable = true;
+    this._startTime = new Time();
+    setInterval(() => {
+      if (this._clockEnable) {
+        var currentTime = new Time();
+        currentTime.minus(this._startTime);
+
+        // this._digit["hours"][0].setNumber(currentTime.millisecondDigit[0], 1);
+        // this._digit["hours"][1].setNumber(currentTime.millisecondDigit[1], 1);
+        // this._digit["minutes"][0].setNumber(Math.floor(minutes / 10), 1);
+        // this._digit["minutes"][1].setNumber(minutes % 10, 1);
+        this._digit["seconds"][0].setNumber(Math.floor(seconds / 10), 1);
+        this._digit["seconds"][1].setNumber(seconds % 10, 1);
+        // this._digit["milliseconds"][0].setNumber(Math.floor(millisecond / 100), 1);
+        // this._digit["milliseconds"][1].setNumber(Math.floor(millisecond / 10), 1);
+        // this._digit["milliseconds"][3].setNumber(seconds % 10, 1);
+      }
+    }, Math.floor(Math.random() * 10));
+  }
+
+  stopClock() {
+    this.clockEnable = false;
+    clearInterval(this.updateClock);
+    clearClock();
+  }
+
+}
+
+$(document).ready(function () {
+  var controls = {
+    startTimer: $("#start-btn"),
+    stopTimer: $("#stop-btn"),
+  }
+
+  var clock = new WodClock();
+
+  function archiveClock(time) {
     var entry = document.createElement('li');
     entry.appendChild(document.createTextNode(time))
     var clockList = document.getElementById('clock-list')
     clockList.appendChild(entry)
- }
+  }
 
- function startTimer() {
-    t = new Date()
-    startTime = t.getTime()
-    timerEnable = true
-    var timer = setTimeout(updateClock, 10)
- }
+  controls.startTimer.click(() => clock.startClock());
+  controls.stopTimer.click(() => clock.stopClock());
 
- function stopTimer() {
-    timerEnable = false
- }
-
- var digitSegments = [
-    [1,2,3,4,5,6],
-    [2,3],
-    [1,2,7,5,4],
-    [1,2,7,3,4],
-    [6,7,2,3],
-    [1,6,7,3,4],
-    [1,6,5,4,3,7],
-    [1,2,3],
-    [1,2,3,4,5,6,7],
-    [1,2,7,3,6]
-]
-
-document.addEventListener('DOMContentLoaded', function() {
-  var _hours = document.querySelectorAll('.hours');
-  var _minutes = document.querySelectorAll('.minutes');
-  var _seconds = document.querySelectorAll('.seconds');
-
-  setInterval(function() {
-    var date = new Date();
-    var hours = date.getHours(), minutes = date.getMinutes(), seconds = date.getSeconds();
-
-    setNumber(_hours[0], Math.floor(hours/10), 1);
-    setNumber(_hours[1], hours%10, 1);
-
-    setNumber(_minutes[0], Math.floor(minutes/10), 1);
-    setNumber(_minutes[1], minutes%10, 1);
-
-    setNumber(_seconds[0], Math.floor(seconds/10), 1);
-    setNumber(_seconds[1], seconds%10, 1);
-  }, 1000);
 });
-
-var setNumber = function(digit, number, on) {
-  var segments = digit.querySelectorAll('.segment');
-  var current = parseInt(digit.getAttribute('data-value'));
-
-  // only switch if number has changed or wasn't set
-  if (!isNaN(current) && current != number) {
-    // unset previous number
-    digitSegments[current].forEach(function(digitSegment, index) {
-      setTimeout(function() {
-        segments[digitSegment-1].classList.remove('on');
-      }, index*45)
-    });
-  }
-
-  if (isNaN(current) || current != number) {
-    // set new number after
-    setTimeout(function() {
-      digitSegments[number].forEach(function(digitSegment, index) {
-        setTimeout(function() {
-          segments[digitSegment-1].classList.add('on');
-        }, index*45)
-      });
-    }, 250);
-    digit.setAttribute('data-value', number);
-  }
-}
-
--- nomad/workout/views.py
+++ nomad/workout/views.py
@@ -1,56 +1,64 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.views.generic import (
+    ListView, FormView
+)
 
 # Create your views here.
 from .forms import (
-   WorkoutBuilderForm,
+    WorkoutBuilderForm,
 )
 from .models import (
-   Movement,
-   Workout,
+    Movement,
+    Workout,
 )
-from django.views.generic import (
-   ListView, FormView
-)
+
 
 class WorkoutListView(ListView):
-   template_name="workout/movement_list.html"
-   model = Movement
+    template_name = "workout/movement_list.html"
+    model = Movement
 
-   def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      # context['now'] = timezone.now()
-      return context
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # context['now'] = timezone.now()
+        return context
+
 
 def movementsView(request):
-   context = {
-      "movements": Movement.objects.all(),   
-      "equipment": Equipment.objects.all(),
-   }
-   return render(request, "workout/movements.html", context)
+    context = {
+        "movements": Movement.objects.all(),
+        "equipment": Equipment.objects.all(),
+    }
+    return render(request, "workout/movements.html", context)
+
 
 def workoutsView(request):
-   context = {
-      "workouts":  Workout.objects.all(),  
-   }
-   return render(request, "workout/workouts.html", context)
+    context = {
+        "workouts": Workout.objects.all(),
+    }
+    return render(request, "workout/workouts.html", context)
+
 
 class WorkoutBuilderFormView(FormView):
-   template_name = "workout/workout_builder.html"
-   form_class = WorkoutBuilderForm
-   success_url = '/thanks/'
+    template_name = "workout/workout_builder.html"
+    form_class = WorkoutBuilderForm
+    success_url = '/thanks/'
+
 
 def workoutBuilderView(request):
-   context = {
-      "workouts":  Workout.objects.all(),
-      "movements":  Movement.objects.all(),
-   }
-   return render(request, "workout/workout_builder.html", context)
+    context = {
+        "workouts": Workout.objects.all(),
+        "movements": Movement.objects.all(),
+    }
+    return render(request, "workout/workout_builder.html", context)
+
 
 def accountView(request):
-   return render(request, "workout/account.html")
+    return render(request, "workout/account.html")
+
 
 def clockView(request):
-   return render(request, "workout/clock.html")
+    return render(request, "workout/clock.html")
+
 
 def loginView(request):
-   return render(request, "workout/login.html")
+    return render(request, "workout/login.html")
